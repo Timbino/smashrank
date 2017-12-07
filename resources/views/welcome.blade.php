fcfa98e3e04bc3fa854ca2cad23a38cc8c42e28a
Timbino <timridderhof@gmail.com>
2017-12-07 22:33:31 +0100

    auth integratie

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,15 +79,15 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    SmashSwag
                 </div>
 
                 <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                    <a href="">Account</a>
+                    <a href="">Ironman</a>
+                    <a href=""></a>
+                    <a href=""></a>
+                    <a href=""></a>
                 </div>
             </div>
         </div>
